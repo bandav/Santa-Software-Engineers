b6768d62441d7ed273e9eb26a6bc235fe7914fd2
bandav <vindhyabanda28@gmail.com>
2022-12-07 17:52:41 -0500

    DB updates for join game functionality

--- project/models.py
+++ project/models.py
@@ -5,27 +5,22 @@ from . import db
 
 # Relations
 
-###CREATE TABLE gift_choice (
-###gift_id integer, // FK to gifts
-###user_id integer, //FK to users
-###PRIMARY KEY (gift_id, user_id)
-###);
-gift_choice = db.Table('gift_choice', 
-    db.Column('gift_id', db.Integer, db.ForeignKey('gift.id'), primary_key=True),
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True)
-)
-
 ###CREATE TABLE players (
 ###user_id integer, //FK to users
 ###game_id integer, //FK to games
 ###PRIMARY KEY (user_id, game_id)
 ###);
-playing = db.Table('playing',
-    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
-    db.Column('game_id', db.Integer, db.ForeignKey('game.id'), primary_key=True)
+joined_game = db.Table('joined_game',
+    db.Column('player_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
 )
 
-# Likes in posts (reaction)
+###CREATE TABLE gift_choice (
+###gift_id integer, // FK to gifts
+###user_id integer, //FK to users
+###PRIMARY KEY (gift_id, user_id)
+###);
+# Likes in gifts, renamed relation so liked_gift for readability
 liked_gift = db.Table('liked_gift',
     db.Column('liking_id', db.Integer, db.ForeignKey('user.id')),
     db.Column('liked_id', db.Integer, db.ForeignKey('gift.id'))
@@ -44,7 +39,6 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(250), unique=True)
     password = db.Column(db.String(250))
     displayname = db.Column(db.String(250))
-    gift_choice = db.relationship('Gift', secondary=gift_choice, lazy='subquery', backref=db.backref('users', lazy=True))
 
     liked = db.relationship('Gift', secondary=liked_gift,backref=db.backref('liked_gift', lazy='dynamic'), lazy='dynamic')
     
@@ -58,7 +52,21 @@ class User(UserMixin, db.Model):
 
     def is_liking(self, gift):
         return self.liked.filter(
-        liked_gift.c.liked_id == gift.id).count() > 0  
+        liked_gift.c.liked_id == gift.id).count() > 0 
+
+    joined = db.relationship('Game', secondary=joined_game,backref=db.backref('joined_game', lazy='dynamic'), lazy='dynamic')
+    
+    def join(self, game):
+        if not self.is_playing(game):
+            self.joined.append(game)
+
+    def unjoin(self, game):
+        if self.is_playing(game):
+            self.joined.remove(gift)
+
+    def is_liking(self, game):
+        return self.joined.filter(
+        joined_game.c.game_id == game.id).count() > 0   
 
 ###CREATE TABLE gifts (
 ###gift_id integer PRIMARY KEY,
