122e99e95100e468b3f28b45f58fa03b2d1fb580
Sofia <sofia@pancake.live>
2022-12-08 11:33:10 -0500

    saving progress sm

--- project/likes.py
+++ project/likes.py
@@ -9,9 +9,22 @@ likes = Blueprint('likes', __name__)
 
 @likes.route('/liked_gifts')
 def liked_gifts():
+
+    # likes_list = get_all_likes()
+    testing = liked_gift.query.filter_by(liking_id=current_user.id).first()
+    print(testing)
+
     print(current_user.username)
+    print(current_user.id)
     return render_template('likes.html')
 
+def get_all_likes():
+    all_likes = liked_gift.query.filter_by(liking_id=current_user.id)
+    curr_user = User.query.filter_by(username=current_user.username).first()
+    result = []
+    for liked_gift in all_likes:
+      result.append(liked_gifts.liked_id)
+    return result 
 
 
     
