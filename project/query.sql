fc764830db1a5087bd1e9b1f5a0a20f32567e523
Sofia <sofia@pancake.live>
2022-12-07 23:03:36 -0500

    added My Likes button

--- project/query.sql
+++ project/query.sql
@@ -1,7 +1,9 @@
 SELECT * FROM gift;
 
+SELECT * FROM liked_gift;
 
-DELETE FROM gift WHERE id=2;
 
-INSERT INTO gift VALUES(1, "YSL Tote", 1000, "Pretty", "", 0);
-INSERT INTO gift VALUES(2, "Teddy Bear", 10, "So cute", "", 0);
+DELETE FROM gift WHERE id=1;
+
+INSERT INTO gift VALUES(0, "YSL Tote", 1000, "Pretty", "", 0);
+INSERT INTO gift VALUES(1, "Teddy Bear", 10, "So cute", "", 0);
