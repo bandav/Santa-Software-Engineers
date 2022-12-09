307f3eabb3a0b9fba9546d895ad374a5ca2471d9
Weiyi Tan <weiyi@pal-nat186-165-245.itap.purdue.edu>
2022-12-08 20:43:03 -0500

    added Dislike for liked gifts list

--- project/likes.py
+++ project/likes.py
@@ -2,6 +2,8 @@ from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask import session as cur_session
 from flask_login import login_required, current_user
 from .models import User, Gift
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -28,9 +30,47 @@ def get_liked_gifts(id):
         result.append(gift.id)
     return result
 
+@likes.route('/like_gift/<id>')
+def like_gift(id):
+  url = db.engine.url
+  engine = create_engine(url)
+  session = Session(bind=engine)
+  session.connection(execution_options={"isolation_level": "READ UNCOMMITTED"})  
+  gift = Gift.query.filter_by(id=id).first()
+  if current_user.is_liking(gift):
+      flash("You've already liked this gift!")
+      return redirect(url_for('main.profile'))
+  gift.likes += 1
+  current_user.like(gift)
+  session.commit()
+  db.session.commit()
+  if 'url' in cur_session:
+    return redirect(cur_session['url'])
+  else:
+    return redirect(url_for('main.profile', id=id))
+
+@likes.route('/unlike_gift/<id>')
+def unlike_gift(id):
+  url = db.engine.url
+  engine = create_engine(url)
+  session = Session(bind=engine)
+  session.connection(execution_options={"isolation_level": "READ UNCOMMITTED"})  
+  gift = Gift.query.filter_by(id=id).first()
+  if gift is None:
+      return redirect(url_for('index', id=id))
+  gift.likes -= 1
+  current_user.unlike(gift)
+  session.commit()
+  db.session.commit()
+  if 'url' in cur_session:
+    return redirect(cur_session['url'])
+  else:
+    return redirect(url_for('main.profile', id=id))
+
 def gift_to_html(gift_id):
     cur_session['url'] = request.url
     obj = Gift.query.filter_by(id=gift_id).first()
+    likes = obj.likes
 
     html_string_base = "<div class=\"box\"> \
         <article class=\"media\">\
@@ -46,15 +86,26 @@ def gift_to_html(gift_id):
 
     
 
-    html_string_liked = " <div class=\"level-right\">  <form action=\"/unjoin_game/"+str(gift_id)+"\">\
-                <button class=\"button is-block is-black is-medium is-fullwidth\" button style=\"margin:5px\">Unlike Gift</button>\
+    html_string_liked = "<div class=\"level-right\">\
+              <form action=\"/unlike_gift/"+str(gift_id)+"\">\
+                <button class=\"button is-block is-black is-normal\" button style=\"margin:5px\">Dislike</button>\
               </form>"
 
+    html_string_unliked = "<div class=\"level-right\">\
+                <form action=\"/like_gift/"+str(gift_id)+"\">\
+                  <button class=\"button is-block is-black is-normal\" button style=\"margin:5px\">Like</button>\
+                </form>" 
+
     if current_user.is_liking(obj):
-      html_string_base += html_string_liked
-    # else:
-    #   if (obj.num_active_players < obj.max_capacity):
-    #     html_string_base += html_string_unjoined 
+        html_string_base += html_string_liked
+    else:
+        html_string_base += html_string_unliked 
+
+    html_string_base += "<div class=\"level-left\">\
+              <p>\
+                Likes: " + str(likes) + "</p>\
+              </div>\
+            </nav>"
   
 
     # Finish off whatever button state the post had
--- project/query.sql
+++ project/query.sql
@@ -9,9 +9,11 @@ SELECT * FROM liked_gift WHERE liked_id = current_user.username
 SELECT * FROM liked_gift WHERE liking_id = 1;
 
 
+DELETE FROM gift WHERE id=0;
 DELETE FROM gift WHERE id=1;
 
-DELETE FROM liked_gift where liking_id=1;
+DELETE FROM liked_gift;
 
-INSERT INTO gift VALUES(0, "YSL Tote", 1000, "Pretty", "", 0);
-INSERT INTO gift VALUES(1, "Teddy Bear", 10, "So cute", "", 0);
+INSERT INTO gift VALUES(0, "YSL Tote", 1000, "Pretty", "https://www.nordstrom.com/s/micro-toy-croc-embossed-patent-leather-shopper/6989910?color=1000+NERO&country=US&currency=USD&utm_source=google&utm_medium=organic&utm_campaign=seo_shopping&utm_channel=low_nd_seo_shopping", 0);
+INSERT INTO gift VALUES(1, "Teddy Bear", 10, "So cute", "https://www.amazon.com/GUND-Ginger-Teddy-Stuffed-Animal/dp/B01NBKTSZ7/ref=asc_df_B01NBKTSZ7/?tag=hyprod-20&linkCode=df0&hvadid=312118126503&hvpos=&hvnetw=g&hvrand=16521001533200412149&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9016722&hvtargid=pla-525309281401&psc=1", 0);
+INSERT INTO gift VALUES(3, "Six of Crows", 7, "Gripping novel about a heist!", "https://www.amazon.com/Six-of-Crows-Leigh-Bardugo-audiobook/dp/B012BNM1LO/ref=sr_1_1?crid=2JDKQGSJM84Y9&keywords=six+of+crows&qid=1670535367&sprefix=six+of+crow%2Caps%2C134&sr=8-1", 0);
