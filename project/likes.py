6261f5b88f064b35503aba695ad805ef6612bbff
Sofia <sofia@pancake.live>
2022-12-08 19:50:18 -0500

    gift list done

--- project/likes.py
+++ project/likes.py
@@ -1,30 +1,66 @@
 from flask import Blueprint, redirect, render_template, request, url_for, flash
 from flask import session as cur_session
 from flask_login import login_required, current_user
-from .models import User
+from .models import User, Gift
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
 
 likes = Blueprint('likes', __name__)
 
-@likes.route('/liked_gifts')
-def liked_gifts():
+@likes.route('/disp_liked_gifts/<id>/<gift_num>')
+def disp_liked_gifts(id, gift_num):
+    gift_list = get_liked_gifts(id)
+    if (not gift_list or len(gift_list) == 0):
+        return "You have no liked gifts. Go back and like one!"
+    gift_num = int(gift_num)
+    list_len = len(gift_list)
+    while (gift_num >= list_len):
+        gift_num -= 1
+    gift_html = gift_to_html(gift_list[gift_num])
 
-    # likes_list = get_all_likes()
-    testing = liked_gift.query.filter_by(liking_id=current_user.id).first()
-    print(testing)
+    return render_template('likes.html', id=id, gift_num = gift_num, gift_html = gift_html, list_len=list_len)
 
-    print(current_user.username)
-    print(current_user.id)
-    return render_template('likes.html')
-
-def get_all_likes():
-    all_likes = liked_gift.query.filter_by(liking_id=current_user.id)
-    curr_user = User.query.filter_by(username=current_user.username).first()
+def get_liked_gifts(id):
+    all_gifts = Gift.query.all()
     result = []
-    for liked_gift in all_likes:
-      result.append(liked_gifts.liked_id)
-    return result 
+    for gift in all_gifts:
+      if current_user.is_liking(gift):
+        result.append(gift.id)
+    return result
 
+def gift_to_html(gift_id):
+    cur_session['url'] = request.url
+    obj = Gift.query.filter_by(id=gift_id).first()
+
+    html_string_base = "<div class=\"box\"> \
+        <article class=\"media\">\
+          <div class=\"media-content\">\
+            <div class=\"content\">\
+              <p>\
+                <strong>" + str(obj.gift_name) + "</strong>\
+                <br>" + "Price: " + str(obj.price) + "</p>\
+                <br>" + "Description: " + str(obj.description) + "</p>\
+            </div>\
+        </article>\
+        </div>"
 
     
+
+    html_string_liked = " <div class=\"level-right\">  <form action=\"/unjoin_game/"+str(gift_id)+"\">\
+                <button class=\"button is-block is-black is-medium is-fullwidth\" button style=\"margin:5px\">Unlike Gift</button>\
+              </form>"
+
+    if current_user.is_liking(obj):
+      html_string_base += html_string_liked
+    # else:
+    #   if (obj.num_active_players < obj.max_capacity):
+    #     html_string_base += html_string_unjoined 
+  
+
+    # Finish off whatever button state the post had
+    html_string_base += "</nav>"
+
+    # Finish off the whole html
+    html_string_base += "</div>"
+
+    return html_string_base
